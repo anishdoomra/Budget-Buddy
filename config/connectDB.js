cf6b54ab41b4b01ff5874a0f0713e2e474f2fcdb
anishdoomra <122442347+anishdoomra@users.noreply.github.com>
2024-09-07 16:51:08 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://anishdoomra12124:SGEKgNxPQy6dqF3C@cluster0.lasivic.mongodb.net/');
+    
 
     console.log(`Server running on ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
